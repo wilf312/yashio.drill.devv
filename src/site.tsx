40980e51d1dcfa3c1e938008f963b465fcec76e2
OkadaGenya <g.okada.wilf+github@gmail.com>
2019-10-15 01:53:43 +0900

    init

---
+++ src/site.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>
+    <h1>hello world!</h1>
+  </div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
